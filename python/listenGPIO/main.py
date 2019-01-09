b1061a9e114831bef38f001432af294c03df29a8
Nathan Costa <nathan@costascodes.com>
2019-01-08 22:30:29 -0500

    Added functionality to the second button. Created the MonitorController class

--- python/listenGPIO/main.py
+++ python/listenGPIO/main.py
@@ -4,15 +4,16 @@ import time
 from neopixel import *
 import RPi.GPIO as GPIO
 from classes.LEDStripController import LEDStripController
+from classes.MonitorController import MonitorController
 
 # LED strip configuration:
 LED_COUNT      = 112     # Number of LED pixels.
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-MAX_INTENSITY  = 255
+MAX_INTENSITY  = 50
 
 #GPIO pin configuration
 LED_STRIP_PIN	= 18
@@ -29,30 +30,39 @@ BUTTON_TWO_LAST = 0.0
 
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(BUTTON_ONE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_TWO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_STRIP_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
 
-stripController = LEDStripController(strip, LED_COUNT, MAX_INTENSITY)
+ledStripController = LEDStripController(strip, LED_COUNT, MAX_INTENSITY)
+monitorController = MonitorController()
 
 def handleButtonOne():
 	global BUTTON_ONE_LAST
 	if time.time() - BUTTON_ONE_LAST > BUTTON_DOWN_TIME:
 		BUTTON_ONE_LAST = time.time()
-		stripController.toggleLEDs()
+
+		ledStripController.toggleLEDs()
 
 def handleButtonTwo():
 	global BUTTON_TWO_LAST
 	if time.time() - BUTTON_TWO_LAST > BUTTON_DOWN_TIME:
 		BUTTON_TWO_LAST = time.time()
 
+		monitorController.toggleMonitor()
+
 if __name__ == '__main__':
-	stripController.resetAll()
+	ledStripController.resetAll()
 
 	while True:
 		if(GPIO.input(BUTTON_ONE_PIN)):
 			handleButtonOne()
+
+		if(GPIO.input(BUTTON_TWO_PIN)):
+			handleButtonTwo()
 
 		time.sleep(0.05)
 
